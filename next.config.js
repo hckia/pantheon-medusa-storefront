62c2475303a625bf1227c5d0aa73f0d1d9bab141
Cyrus Kia <hckia1@gmail.com>
2022-10-31 11:51:26 -0400

    rebuilt to exclude old domains

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-file-server.s3.us-east-2.amazonaws.com", "localhost", "live-hack-medusa-storefront-next.appa.pantheon.site", "medusa-backend-hack22.onrender.com"],
+    domains: ["medusa-file-server.s3.us-east-2.amazonaws.com", "localhost", "store.cyruskia.com", "api.cyruskia.com"],
   },
 })
 
